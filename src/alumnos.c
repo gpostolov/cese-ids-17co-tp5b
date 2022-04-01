736991486c57b32d36f8b18172678d9f750e4818
Gabriel <gpostolov@hotmail.com>
2022-03-31 22:01:21 -0300

    Se agregan los datos del alumno POSTOLOV, Gabriel

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s GABRIEL_POSTOLOV = {
+    .apellidos = "POSTOLOV",
+    .nombres = "Gabriel Maximiliano",
+    .documento = "32.764.711",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &GABRIEL_POSTOLOV,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
